3cac94839bbe523417d65228471a0b234e627a0a
Security2431 <artem2431@gmail.com>
2023-12-31 13:08:14 +0200

    fix: delete duplicate folder wrapper

---
+++ examples/with-tailwindcss/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReactTextareaAutocomplete from "@security2431/react-textarea-autocomplete";
+
+function App() {
+  return (
+    <>
+      <ReactTextareaAutocomplete />
+    </>
+  );
+}
+
+export default App;
