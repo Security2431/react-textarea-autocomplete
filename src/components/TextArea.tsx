de37a03df0e54e9da01e24c3ec8ba454d85d4b83
Security2431 <artem2431@gmail.com>
2023-12-31 09:50:40 +0200

    feat: configure tailwindcss

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,8 @@
+export const TextArea = () => {
+  return (
+    <textarea
+      className="placeholder:text-green-500"
+      placeholder="Start typing..."
+    ></textarea>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "react-textarea-autocomplete",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
   plugins: [react()],
-})
+});
